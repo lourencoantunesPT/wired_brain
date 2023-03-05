a2690a117da8e52512e897ebfc8d26bc2306d279
louro <lourenco.antunes.pt@gmail.com>
2023-03-05 23:12:49 +0000

    start rust

---
+++ HelloWorld1/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let have_boarding_pass: bool = true;
+    let have_id: bool = false;
+    let can_board:bool = have_boarding_pass && have_id;
+
+    println!("Boarding Pass: {}, ID: {}", have_boarding_pass, have_id);
+    println!("Can board plane: {}", can_board);
+
+    println!("Hello, world!");
+}
